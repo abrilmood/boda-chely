e59b73c25f623147e87e1962c9bcdeecc63e1bc2
Abri <abrilmood@gmail.com>
2024-10-25 16:46:38 -0600

    Changed api url

--- js/scripts.js
+++ js/scripts.js
@@ -192,7 +192,7 @@ $(document).ready(function () {
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Tu código de invitación es.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbzSezAmU_dVQAxfkyAoyDFa1SlwpVRz9r5dRQw57r3DoGlQYGZaa7aLPrgOXNWl19fMsg/exec', data)
                 .done(function (data) {
